a13d9e7255e0244abacdde9b3fcf9465c7bdadd1
yurii <yuriydorosh2005@gmail.com>
2023-07-23 15:08:33 +0300

    UPDATE: new calc struct and activate function with settings of the app window

--- calculator.c
+++ calculator.c
@@ -5,6 +5,30 @@
 #include<stdbool.h>
 
 
+typedef struct{
+	GtkWidget *window;
+	GtkWidget *grid;
+	GtkWidget *button[17];
+}calc;
+
+GtkWidget *box;
+
+static void activate(GtkApplication *app, gpointer user_data);
+
+static void activate(GtkApplication *app, gpointer user_data){
+	calc widget;
+
+	widget.window = gtk_application_window_new(app);
+	gtk_window_set_position(GTK_WINDOW(widget.window),GTK_WIN_POS_CENTER);
+	gtk_window_set_title(GTK_WINDOW(widget.window),"Calculator");
+	gtk_window_set_default_size(GTK_WINDOW(widget.window), 200, 200);
+	gtk_container_set_border_width(GTK_CONTAINER(widget.window),10);
+
+    gtk_widget_show_all(widget.window);
+}
+
+
+
 int main(int argc, char **argv){
 	GtkApplication *app; // Declare a pointer to a GtkApplication object
 
