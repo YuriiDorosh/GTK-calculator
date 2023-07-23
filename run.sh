10f0cfcd2a15a9258081bf930afb16fea967beea
yurii <yuriydorosh2005@gmail.com>
2023-07-23 19:09:07 +0300

    ADD: bash file for building and running the project

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Install the requirements
+sudo apt update
+sudo apt install -y gcc
+sudo apt install -y pkg-config
+sudo apt-get install -y libgtk-3-dev
+
+# Compile the project
+gcc calculator.c -o calc `pkg-config --cflags --libs gtk+-3.0`
+
+# Run the project
+./calc
